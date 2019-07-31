a1ad296a5769ff2623fd0cbef0508e347d3a06d6
abimael andrade <abimaelc.andrade@gmail.com>
2019-07-30 23:30:58 -0300

    feature: create global css reset.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import GlobalStyles from "./styles/global";
+
 function App() {
-  return <div className="App" />;
+  return <GlobalStyles />;
 }
 
 export default App;
