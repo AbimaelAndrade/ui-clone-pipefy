478e08c0fbe1055c44b172eda9421ffcfba6c183
abimael andrade <abimaelc.andrade@gmail.com>
2019-08-01 10:06:45 -0300

    Updates

---
+++ precache-manifest.f2777a4b55af39a34883c9cd8b99d76e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9a941f64355eb59835bce920a1e003",
+    "url": "/ui-clone-pipefy/index.html"
+  },
+  {
+    "revision": "56cd88b9f9a641bd7443",
+    "url": "/ui-clone-pipefy/static/js/2.adcdcc15.chunk.js"
+  },
+  {
+    "revision": "43b01ee1ec8feacdcab7",
+    "url": "/ui-clone-pipefy/static/js/main.109380ef.chunk.js"
+  },
+  {
+    "revision": "0427cdc0ab63ac33c622",
+    "url": "/ui-clone-pipefy/static/js/runtime~main.1890c2c7.js"
+  }
+]);
